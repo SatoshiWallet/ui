c7b0f9a2c46e873c71d52214562282d536d1256b
hosekhoshtaghaza <hossein@satoshipoint.co.uk>
2020-06-02 11:54:05 +0800

    Set status bar transparent

--- src/components/scenes/LoginScene.js
+++ src/components/scenes/LoginScene.js
@@ -77,6 +77,6 @@ const styles = StyleSheet.create({
     flex: 1,
     position: 'relative',
     paddingTop: StatusBar.currentHeight,
-    backgroundColor: THEME.COLORS.PRIMARY
+    backgroundColor: THEME.COLORS.TRANSPARENT
   }
 })
